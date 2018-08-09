456d3965fcf5f25532c9640b760f8e945079e32c
David <davidgcastro93@gmail.com>
2018-08-09 17:01:59 -0400

    styled form

--- client/components/AddTool.js
+++ client/components/AddTool.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default class AddTool extends Component {
   constructor() {
@@ -20,86 +20,33 @@ export default class AddTool extends Component {
 
   render() {
     return (
-      <Form onChange={this.handleChange()}>
+      <Form className="form" onChange={this.handleChange(event)}>
         <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input
-            type="email"
-            name="email"
-            id="exampleEmail"
-            placeholder="with a placeholder"
-          />
+          <Label for="name">Name</Label>
+          <Input type="text" name="name" placeholder="Enter Product Name" />
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="examplePassword"
-            placeholder="password placeholder"
-          />
+          <Label for="Description">Description</Label>
+          <Input type="textarea" name="description" />
         </FormGroup>
         <FormGroup>
-          <Label for="exampleSelect">Select</Label>
+          <Label for="Category">Category</Label>
           <Input type="select" name="select" id="exampleSelect">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
+            <option>General</option>
+            <option>Wood Work</option>
+            <option>Farm Work</option>
+            <option>Carpentry</option>
+            <option>Metal Work</option>
           </Input>
         </FormGroup>
+
         <FormGroup>
-          <Label for="exampleSelectMulti">Select Multiple</Label>
-          <Input
-            type="select"
-            name="selectMulti"
-            id="exampleSelectMulti"
-            multiple>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
+          <Label for="Price">Price</Label>
+          <Input type="text" name="price" placeholder="Enter Product Price" />
         </FormGroup>
         <FormGroup>
-          <Label for="exampleText">Text Area</Label>
-          <Input type="textarea" name="text" id="exampleText" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleFile">File</Label>
-          <Input type="file" name="file" id="exampleFile" />
-          <FormText color="muted">
-            This is some placeholder block-level help text for the above input.
-            It's a bit lighter and easily wraps to a new line.
-          </FormText>
-        </FormGroup>
-        <FormGroup tag="fieldset">
-          <legend>Radio Buttons</legend>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Option one is this and
-              that—be sure to include why it's great
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Option two can be something
-              else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-          <FormGroup check disabled>
-            <Label check>
-              <Input type="radio" name="radio1" disabled /> Option three is
-              disabled
-            </Label>
-          </FormGroup>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" /> Check me out
-          </Label>
+          <Label for="image">Image</Label>
+          <Input type="text" name="image" placeholder="Enter Product image" />
         </FormGroup>
         <Button>Submit</Button>
       </Form>
--- client/components/Main.js
+++ client/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from '../components/nav';
 import AddTool from '../components/AddTool';
 import '../styles/global.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Main = () => {
   return (
